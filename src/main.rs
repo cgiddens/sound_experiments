88a35aca6fda950554bcba26072bfce7a4fc76ef
Calvin Giddens <cgidd@macbook-air-2.local>
2025-07-13 17:09:43 -0500

    - clippy update

--- src/main.rs
+++ src/main.rs
@@ -105,6 +105,7 @@ impl Source for WavetableOscillator {
     }
 }
 
+#[allow(non_snake_case)]
 fn main() {
     let wave_table_size = 64;
     let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
